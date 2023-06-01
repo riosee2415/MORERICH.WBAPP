cc2cd35865e21b23ccbf4c38bf93fa3eb8a53423
4leafjhj <4leaf.jhj@gmail.com>
2023-06-01 14:49:43 +0900

    faq

--- front/pages/customer/[id].jsx
+++ front/pages/customer/[id].jsx
@@ -25,18 +25,6 @@ import { Select } from "antd";
 import styled from "styled-components";
 import Link from "next/dist/client/link";
 
-const List = styled(Wrapper)`
-  flex-direction: row;
-  justify-content: space-between;
-  border-bottom: 1px solid ${Theme.grey3_C};
-  padding: 20px 0;
-
-  &:hover {
-    cursor: pointer;
-    border-bottom: 1px solid ${Theme.basicTheme_C};
-  }
-`;
-
 const Index = () => {
   ////// GLOBAL STATE //////
   const [currentTab, setCurrentTab] = useState(0);
@@ -73,28 +61,50 @@ const Index = () => {
                 fontSize={`16px`}
                 isActive={currentTab === 0}
                 onClick={() => setCurrentTab(0)}
+                margin={`0 26px 0 0`}
               >
                 공지사항
               </Wrapper>
-            </Wrapper>
-            <Wrapper width={`auto`} fontSize={`16px`} color={Theme.grey2_C}>
-              FAQ
+              <Wrapper
+                cursor={`pointer`}
+                width={`auto`}
+                fontSize={`16px`}
+                color={Theme.grey2_C}
+                isActive={currentTab === 1}
+                onClick={() => setCurrentTab(1)}
+              >
+                FAQ
+              </Wrapper>
             </Wrapper>
           </Wrapper>
-          <Wrapper width={`75%`}>
+          <Wrapper width={width < 900 ? `90%` : `75%`} margin={`0 0 100px`}>
             <Wrapper al={`flex-start`} margin={`0 0 20px`}>
-              <Text fontSize={`26px`} fontWeight={`bold`}>
+              <Text
+                fontSize={width < 900 ? `22px` : `26px`}
+                fontWeight={`bold`}
+              >
                 공지사항
               </Text>
             </Wrapper>
-            <Wrapper dr={`row`} ju={`space-between`} margin={`0 0 20px`}>
-              <Wrapper width={`auto`} color={Theme.grey_C}>
+            <Wrapper
+              dr={`row`}
+              ju={`space-between`}
+              margin={width < 900 ? `0 0 10px` : `0 0 20px`}
+            >
+              <Wrapper
+                width={`auto`}
+                color={Theme.grey_C}
+                margin={width < 900 ? `0 0 10px` : `0`}
+              >
                 000개의 게시글이 존재합니다.
               </Wrapper>
-              <Wrapper width={`315px`} position={`relative`}>
+              <Wrapper
+                width={width < 900 ? `100%` : `315px`}
+                position={`relative`}
+              >
                 <TextInput
                   type={`text`}
-                  width={`315px`}
+                  width={width < 900 ? `100%` : `315px`}
                   height={`40px`}
                   placeholder={`검색어를 입력해주세요.`}
                   padding={`0 40px 0 20px`}
@@ -118,8 +128,8 @@ const Index = () => {
               dr={`row`}
               ju={`flex-start`}
               borderTop={`1px solid ${Theme.black_C}`}
-              padding={`30px 24PX`}
-              fontSize={`19px`}
+              padding={width < 900 ? `20px 14px` : `30px 24PX`}
+              fontSize={width < 900 ? `15px` : `19px`}
             >
               제목을 입력해주세요.
             </Wrapper>
@@ -129,10 +139,19 @@ const Index = () => {
               margin={`0 0 30px`}
             >
               <Wrapper dr={`row`} ju={`flex-start`}>
-                <Text color={Theme.grey_C} margin={`0 10px 0 0`}>
+                <Text
+                  fontSize={width < 900 ? `13px` : `14px`}
+                  color={Theme.grey_C}
+                  margin={`0 10px 0 0`}
+                >
                   작성일
                 </Text>
-                <Text margin={`0 55px 0 0`}>23.05.25</Text>
+                <Text
+                  fontSize={width < 900 ? `13px` : `14px`}
+                  margin={`0 55px 0 0`}
+                >
+                  23.05.25
+                </Text>
                 <Text color={Theme.grey_C} margin={`0 10px 0 0`}>
                   조회수
                 </Text>
@@ -145,10 +164,17 @@ const Index = () => {
               fontSize={`16px`}
               margin={`0 0 70px`}
             >
-              <Text margin={`0 0 42px`}>
+              <Text
+                fontSize={width < 900 ? `13px` : `14px`}
+                margin={`0 0 42px`}
+              >
                 내용이 들어올 곳입니다. 더욱 길어지게 된다면....
               </Text>
-              <Image alt="notice" src={``} />
+              <Image
+                width={`60%`}
+                alt="notice"
+                src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/morerich/assets/images/main-page/img6.png`}
+              />
             </Wrapper>
             <Wrapper borderTop={`1px solid ${Theme.grey3_C}`}>
               <Link href={`/customer/notice`}>
